d794ef35a248eeaf8520c9215e8bfcdd3ff44980
Adetunji Aramide <adetunjiaramide@gmail.com>
2025-07-28 03:45:16 +0100

    Initial commit

---
+++ lib/email-service-cdk-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class EmailServiceCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'EmailServiceCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
